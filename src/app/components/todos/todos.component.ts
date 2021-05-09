2a486a1f60a3533cf202b3ad2a62819020c35895
luciano.ramello <luciano.ramello@vass.es>
2021-05-09 10:17:32 +0200

    add todo model

--- src/app/components/todos/todos.component.ts
+++ src/app/components/todos/todos.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Todo} from "../../models/Todo"
 
 @Component({
   selector: 'app-todos',
@@ -7,9 +8,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TodosComponent implements OnInit {
 
+  todos:Todo[];
   constructor() { }
 
   ngOnInit(): void {
+
+    this.todos = [
+      {
+        content:"First todo",
+        completed:false
+      },
+      {
+        content: "second todo",
+        completed: true
+      }
+    ]
   }
 
 }
