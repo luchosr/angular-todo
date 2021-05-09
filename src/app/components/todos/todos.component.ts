33bf3843435a613a122a99f5a95c3925d64a90c0
luciano.ramello <luciano.ramello@vass.es>
2021-05-09 11:00:59 +0200

    add remove todo method

--- src/app/components/todos/todos.component.ts
+++ src/app/components/todos/todos.component.ts
@@ -26,12 +26,17 @@ export class TodosComponent implements OnInit {
   }
 
 
-  toggleDone(id){
+  toggleDone(id:number){
     this.todos.map((v,i)=>{
       if(i == id) v.completed = !v.completed;
 
       return v;
     })
   }
+
+  deleteTodo(id:number){
+    this.todos = this.todos.filter((v,i) => i !== id);
+  }
+
 
 }
